0b25f261381162ba8b546280f3ea5bed21658fcd
4NF0 <148747658+4NF0@users.noreply.github.com>
2023-11-30 11:24:13 +0700

    commit

---
+++ Koordinat.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihan54koordinat;
+
+/**
+ *
+ * @author nm
+ */
+public class Koordinat {
+    protected int x;
+    protected int y;
+    
+    public Koordinat(int x, int y){
+        
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
